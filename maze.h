09db49795e04732a0acc3407adf34062e58d2d5c
jimlorm <jimlormmartinez@gmail.com>
2026-07-22 12:56:30 +0800

    feat: implement `loadMaze` function

--- maze.h
+++ maze.h
@@ -3,7 +3,7 @@
 #include "position.h"
 
 typedef struct {
-    char** map;
+    char map[30][30];
     int height;      
     int width;       
     Position start;
@@ -12,10 +12,68 @@ typedef struct {
 
 Maze* loadMaze(char* filename) 
 {
+    Maze* maze = NULL;
+    FILE* fp;
+    int i, j;
 
+    fp = fopen(filename, "r");
+
+    if (fp != NULL)
+    {
+        maze = (Maze*)malloc(sizeof(Maze));
+        
+        if (maze != NULL)
+        {
+            maze->start.row = -1;
+            maze->start.col = -1;
+            maze->goal.row = -1;
+            maze->goal.col = -1;
+
+            fscanf(fp, "%d %d", &maze->height, &maze->width);
+
+            // skip to the next line
+            fscanf(fp, "%*[^\n]");
+            fscanf(fp, "%*c");
+
+            for (i = 0; i < maze->height; i++)
+            {
+                for (j = 0; j < maze->width; j++)
+                {
+                    fscanf(fp, "%c", &maze->map[i][j]);
+
+                    if (maze->map[i][j] == 'S')
+                    {
+                        maze->start.row = i;
+                        maze->start.col = j;
+                    }
+                    else if (maze->map[i][j] == 'G')
+                    {
+                        maze->goal.row = i;
+                        maze->goal.col = j;
+                    }
+                }
+
+                // skip to the next line
+                fscanf(fp, "%*[^\n]");
+                fscanf(fp, "%*c");
+            }
+
+        }
+        fclose(fp);
+    }
+    else
+    {
+        fprintf(stderr, "Error: Could not open file '%s'\n", filename);
+    }
+
+    return maze;
 }
 
-void freeMaze(Maze** mazePtr)
+void deleteMaze(Maze** mazePtr)
 {
-    
+if (mazePtr != NULL && *mazePtr != NULL)
+    {
+        free(*mazePtr);
+        *mazePtr = NULL;
+    }
 }
