95b9f68e5531cf7f76b0b162c18a708bf0b13723
jimlorm <jimlormmartinez@gmail.com>
2026-07-23 13:46:47 +0800

    refactor: put the declarations to a header file

--- maze.h
+++ maze.h
@@ -1,7 +1,16 @@
+#ifndef MAZE_H
+#define MAZE_H
+
+#define _POSIX_C_SOURCE 199309L
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "position.h"
+#include "stacks.h"
+#include "queue.h"
 
+
+// Maze struct declaration
 typedef struct {
     char map[30][30];
     int height;      
@@ -10,84 +19,11 @@ typedef struct {
     Position goal;
 } Maze;
 
-Maze* loadMaze(char* filename) 
-{
-    Maze* maze = NULL;
-    FILE* fp;
-    int i, j;
+// Function prototypes
+void delay(long int milliseconds); 
+Maze* loadMaze(char* filename);
+void deleteMaze(Maze** mazePtr);
+void displayMaze(Maze* maze);
+void mazeBFS(Maze* maze, Position predecessor[30][30], int* cellsExplored);
 
-    fp = fopen(filename, "r");
-
-    if (fp != NULL)
-    {
-        maze = (Maze*)malloc(sizeof(Maze));
-        
-        if (maze != NULL)
-        {
-            maze->start.row = -1;
-            maze->start.col = -1;
-            maze->goal.row = -1;
-            maze->goal.col = -1;
-
-            fscanf(fp, "%d %d", &maze->height, &maze->width);
-
-            // skip to the next line
-            fscanf(fp, "%*[^\n]");
-            fscanf(fp, "%*c");
-
-            for (i = 0; i < maze->height; i++)
-            {
-                for (j = 0; j < maze->width; j++)
-                {
-                    fscanf(fp, "%c", &maze->map[i][j]);
-
-                    if (maze->map[i][j] == 'S')
-                    {
-                        maze->start.row = i;
-                        maze->start.col = j;
-                    }
-                    else if (maze->map[i][j] == 'G')
-                    {
-                        maze->goal.row = i;
-                        maze->goal.col = j;
-                    }
-                }
-
-                // skip to the next line
-                fscanf(fp, "%*[^\n]");
-                fscanf(fp, "%*c");
-            }
-
-        }
-        fclose(fp);
-    }
-    else
-    {
-        fprintf(stderr, "Error: Could not open file '%s'\n", filename);
-    }
-
-    return maze;
-}
-
-void deleteMaze(Maze** mazePtr)
-{
-    if (mazePtr != NULL && *mazePtr != NULL)
-    {
-        free(*mazePtr);
-        *mazePtr = NULL;
-    }
-}
-
-void displayMaze(Maze* maze)
-{
-    int i, j;
-
-    for (i = 0; i < maze->height; i++)
-    {
-        for (j = 0; j < maze->width; j++)
-        {
-            printf("%c", maze->map[i][j]);
-        }
-        printf("\n");
-    }
-}
+#endif // MAZE_H
